87572f8bcc28a0285caf2b15679d5a9194c4557f
Dylan Cole <logboarddjc@gmail.com>
2021-09-07 19:21:55 -0400

    update to simpler file structure lol ;)

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/html/index.html'))
-});
-
 app.post('/api/report', (req, res) => {
     console.log(req.body);
     res.send('Completedd!');
