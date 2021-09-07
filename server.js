17419a40e6637dea0c045d46dab730e517d3caf3
Dylan Cole <logboarddjc@gmail.com>
2021-09-07 16:18:11 -0400

    change file structure and bring in forecast.js file through script tag in index.html

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/html/index.html'))
+})
+
 app.listen(PORT, () => {
     console.log.apply(`Server running on port ${PORT}`);
 });
