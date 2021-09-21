23fb6c05f714f08614474ae9791ff298bac87862
Dylan <logboarddjc@gmail.com>
2021-09-20 22:27:26 -0400

    refactor routes to make windy api request from the backend

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/forecast');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+routes(app);
+
+app.listen(PORT, () => {
+    console.log.apply(`Server running on port ${PORT}`);
+});
